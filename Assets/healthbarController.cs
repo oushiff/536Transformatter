c024022c1522c51a77837df9f55f7febd31a6cac
Dan Luo <luod@usc.edu>
2016-04-16 14:41:07 -0700

    Healthbar added
    health bar added in level 1

---
+++ Assets/healthbarController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class healthbarController : MonoBehaviour {
+	public Image healthBar;
+	private float health;
+	// Use this for initialization
+	void Start () {
+		health = GameManager.Instance.Health;
+		healthBar.rectTransform.localScale = new Vector3 ( health/100, healthBar.rectTransform.localScale.y, healthBar.rectTransform.localScale.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		health = GameManager.Instance.Health;
+		healthBar.rectTransform.localScale = new Vector3 ( health/100, healthBar.rectTransform.localScale.y, healthBar.rectTransform.localScale.z);
+
+	}
+}
