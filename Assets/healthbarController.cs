8b75472a4258b8439ae3a3ab9b89510b73062ea5
Xin Ge <xinge@gexindeMacBook-Pro.local>
2016-04-18 22:08:46 -0700

    Dan: Add twinkle effect to healthbar, Xin: Modified level1.unity so that UI text can update

--- Assets/healthbarController.cs
+++ Assets/healthbarController.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class healthbarController : MonoBehaviour {
 	public Image healthBar;
 	private float health;
+	private int countdown=0;
 	// Use this for initialization
 	void Start () {
 		health = GameManager.Instance.Health;
@@ -12,6 +13,17 @@ public class healthbarController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (health != GameManager.Instance.Health) {
+			healthBar.color = Color.yellow;
+			countdown = 20;
+		} else {
+			if (countdown == 0) {
+				healthBar.color = Color.red;
+			} else {
+				healthBar.color = Color.yellow;
+				countdown--;
+			}
+		}
 		health = GameManager.Instance.Health;
 		healthBar.rectTransform.localScale = new Vector3 ( health/100, healthBar.rectTransform.localScale.y, healthBar.rectTransform.localScale.z);
 
