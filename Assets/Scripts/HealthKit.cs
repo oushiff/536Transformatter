778333188b250c8fd6f5c09073623b4c8da1be5b
Xin Ge <xinge@gexindeMacBook-Pro.local>
2016-04-20 14:55:44 -0700

    Add health kit so that player can regain some health value

---
+++ Assets/Scripts/HealthKit.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthKit : MonoBehaviour {
+	[SerializeField]
+	private float rotateSpeed = 1.0f;
+
+	[SerializeField]
+	private float floatSpeed = 0.5f; // In cycles (up and down) per second
+
+	[SerializeField]
+	private float movementDistance = 0.5f; // The maximum distance the coin can move up and down.
+
+
+	private float startingY;
+	private bool isMovingUp = true;
+
+	void OnTriggerEnter2D(Collider2D collider2d)
+	{
+		if (collider2d.gameObject.tag == "PlayerSnow" || collider2d.gameObject.tag == "PlayerCloud")
+		{
+			GetHealth();
+		}
+	}
+
+	[SerializeField]
+	private float healthAddition = 20;
+
+	private void GetHealth()
+	{
+		GameManager.Instance.Health += healthAddition;
+
+		if (GameManager.Instance.Health > 100) {
+			GameManager.Instance.Health -= 100;
+		}
+
+		Destroy (gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		startingY = transform.position.y;
+		transform.Rotate (transform.up, Random.Range (0f, 360f));
+
+		StartCoroutine (Spin ());
+		StartCoroutine (Float ());
+	}
+
+
+
+	private IEnumerator Float()
+	{
+		while (true)
+		{
+			float newY = transform.position.y + (isMovingUp ? 1 : -1) * 2 * movementDistance * floatSpeed * Time.deltaTime;
+
+			if (newY > startingY + movementDistance)
+			{
+				newY = startingY + movementDistance;
+				isMovingUp = false;
+			}
+			else if (newY < startingY)
+			{
+				newY = startingY;
+				isMovingUp = true;
+			}
+
+			transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+			yield return 0;
+		}
+	}
+	private IEnumerator Spin()
+	{
+		while (true) 
+		{
+			transform.Rotate (transform.up, 360 * rotateSpeed * Time.deltaTime);
+			yield return 0;
+		}
+	}
+}
